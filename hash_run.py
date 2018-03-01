9e457cc91bbceb1adacf346e4f6657c695ca6ade
V4570 <vasilis.tos@gmail.com>
2018-03-02 00:51:14 +0200

    final

--- hash_run.py
+++ hash_run.py
@@ -1,4 +1,9 @@
 import argparse
+from operator import itemgetter
+import sys
+import copy as copy
+
+car_ride = []
 
 
 def read_data(infile):
@@ -6,38 +11,62 @@ def read_data(infile):
 		rows, columns, vehicles, num_rides, bonus, steps = [int(num) for num in fin.readline().split()]
 
 		rides = []
-		for _ in range(num_rides):
+		for i in range(num_rides):
 			ride = [int(num) for num in fin.readline().split()]
 			distance = calc_distance(ride[0], ride[1], ride[2], ride[3])
 			ride.append(distance)
+			ride.append(i)
 			rides.append(ride)
 
 		return rows, columns, vehicles, num_rides, bonus, steps, rides
 
 
-def init_cars(cars):
+def algorithm(data, info_cars):
+	rides = copy.copy(data[6])
+	rides = sorted(rides, key=lambda eval: [eval[4], -(eval[5] - eval[6])])
 
+	# for every ride
+	for r in rides:
+		total_dis = []
+		vehicle = -1
+		# for every car
+		for car_id in range(0, data[2]):
+			total_dis.append(start_of_ride(r, car_id, info_cars))
+		# which car will take the ride
+		vehicle = total_dis.index(min(total_dis))
+		# print (total_dis)
+		if (r[5] > total_dis[vehicle]):
+			step_car(vehicle, r, info_cars, r[7])
+
+
+def init_cars(cars):
+	global car_ride
 	info_cars = []
-	for i in cars:
-		car = [0, 0, 0, []]
+
+	for i in range(0, cars):
+		car = [0, 0, 0]
+		car_ride.append([])
 		info_cars.append(car)
 	return info_cars
 
 
 def step_car(car, ride, info_cars, ride_id):
 	distance = calc_distance(info_cars[car][0], info_cars[car][1], ride[0], ride[1])
-	time = max(ride[4], distance)
+	time = max(ride[4], info_cars[car][2] + distance)
 	total = time + ride[6]
-	info_cars[car][3] = total
+	info_cars[car][2] = total
 	info_cars[car][0] = ride[2]
 	info_cars[car][1] = ride[3]
-	info_cars[3].append(ride_id)
+	car_ride[car].append(ride_id)
 
 
 def start_of_ride(ride, car, info_cars):
 	distance = calc_distance(info_cars[car][0], info_cars[car][1], ride[0], ride[1])
-	time = max(ride[4], distance)
-	return time
+	time = max(ride[4], info_cars[car][2] + distance)
+	if (time + ride[6] < ride[5]):
+		return time + ride[6]
+	else:
+		return sys.maxsize
 
 
 def calc_distance(x1, y1, x2, y2):
@@ -47,10 +76,33 @@ def calc_distance(x1, y1, x2, y2):
 	return d
 
 
+def printData(info_cars):
+	for i in car_ride:
+		print(str(len(i)), *i)
+
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input', type=str, help='input file')
 	infile = parser.parse_args().input
 	data = read_data(infile)
 	info_cars = init_cars(data[2])
-	print(data)
+	algorithm(data, info_cars)
+	printData(info_cars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
