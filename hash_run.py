c7eb8bc0fc652b2e54c667499fc38d33dd40ae68
Vasilis Tosounidis <vasilis.tos@gmail.com>
2018-03-01 19:55:25 +0200

    read implement

--- hash_run.py
+++ hash_run.py
@@ -1,8 +1,24 @@
+import argparse
 
 def read_data(infile):
 
     with open(infile, 'r') as fin:
 
-        X1, X2, X3, X4 = [int(num) for num in fin.readline().split()]
+        R, C, F, N, B, T = [int(num) for num in fin.readline().split()]
 
-        return X1, X2, X3, X4
+        rides = []
+        for _ in range(N):
+            ride = [int(num) for num in fin.readline().split()]
+            rides.append(ride)
+
+
+        return R, C, F, N, B, T, rides
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=str, help='input file')
+    infile = parser.parse_args().input
+    data = read_data(infile)
+    print(data)
+
